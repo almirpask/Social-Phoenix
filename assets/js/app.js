2e3e6c6707a073e3deb47837317c2b6869466fdc
almirpask <almirpask@hotmail.com>
2019-03-16 17:38:58 -0300

    add materialize and setup sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import 'materialize-css/dist/js/materialize.min.js' 
+import 'materialize-css/dist/css/materialize.min.css' 
+
+import css from "../css/app.scss"
+
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -15,3 +19,16 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+// M.AutoInit();
+let dropDownElem = document.getElementById('dropdown-button');
+let dropDownInstances = M.Dropdown.init(dropDownElem, {});
+
+var sideNavElem = document.getElementById('side-bar');
+var sideNavInstances = M.Sidenav.init(sideNavElem, {});
+
+
+
+
+
+
